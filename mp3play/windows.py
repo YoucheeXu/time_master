e0d32e56aad0eaac5912a4eab8300724666005c9
Youchee <youchi.xu@live.com>
2026-04-22 14:34:04 +0800

    fix: play repeatly

--- mp3play/windows.py
+++ mp3play/windows.py
@@ -57,7 +57,8 @@ class AudioClip:
 		start_ms = 0 if not start_ms else start_ms
 		end_ms = self.milliseconds() if not end_ms else end_ms
 		# err, buf = self._mci.directsend('play %s from %d to %d' %(self._alias, start_ms, end_ms))
-		_ = self._mci.directsend('play %s repeat' %self._alias)
+		# _ = self._mci.directsend('play %s repeat' %self._alias)
+		_ = self._mci.directsend('play %s' %self._alias)
 
 	def isplaying(self):
 		return self._mode() == 'playing'
